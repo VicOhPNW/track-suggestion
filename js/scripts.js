591ed8713f06979e5758fcf8533d4e2c6fc71762
Victoria Oh <ohvictori@gmail.com>
2018-08-10 13:23:41 -0700

    Set variables for input values in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
   $("#languageTrack").ready(function() {
 
+  $("button").click(function(event) {
 
+    var gameInput = $("#gameInput").val();
+    var corporation = $("input:radio[name=corporation]:checked").val();
+    var build = $("input:radio[name=build]:checked").val();
+    var hero = $("input:radio[name=hero]:checked").val();
+    var fingers = $("input:radio[name=fingers]:checked").val();
+
+
+
+
+
+
+
+  });
   });
 });
