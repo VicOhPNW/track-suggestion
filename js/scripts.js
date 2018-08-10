53812b46ce07562b2fa6051b1124b4df5c0a01e4
Victoria Oh <ohvictori@gmail.com>
2018-08-10 16:22:48 -0700

    Complete README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
 $(document).ready(function(){
   $("button").click(function(event) {
-  alert("works");
+
+    var game = $("#gameInput").val();
+    var corporation = $("input:radio[name=corporation]:checked").val();
+    var build = $("input:radio[name=build]:checked").val();
+    var hero = $("input:radio[name=hero]:checked").val();
+    var fingers = $("input:radio[name=fingers]:checked").val();
+
+//Hide when remaining result options when result appears
+    // $("li").hide();
+
+// This alert works unsure what's going on below.-
+    alert("work");
+
+// //Reult branching
+//     if (game === "1" && corporation === "3" && build === "6" && hero === "10" && fingers === "15") {
+//       $("#design").show();
+//     } else if (game === "2" && corporation === "4" && build === "7" && hero = "11" && fingers === "16") {
+//       $("#java").show();
+//     } else if
 
 
-//
-//     var game = $("#gameInput").val();
-//     var corporation = $("input:radio[name=corporation]:checked").val();
-//     var build = $("input:radio[name=build]:checked").val();
-//     var hero = $("input:radio[name=hero]:checked").val();
-//     var fingers = $("input:radio[name=fingers]:checked").val();
-//
-//     event.preventDefault()
-//
-//     $("li").hide();
-//
-//     // if (game === "1"
-//
-//
-//
-//
-//
-//
-//
+
+
+
+
+
+
+
 
 
     event.preventDefault();
