705c66fa69876d59f6582603041a8b8963533465
Victoria Oh <ohvictori@gmail.com>
2018-08-10 16:12:01 -0700

    Branching does not show results. Play with dev tools to determine error for last few hours.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
-$(document).ready(function() {
-  $("#languageTrack").ready(function() {
-
+$(document).ready(function(){
   $("button").click(function(event) {
-
-    var gameInput = $("#gameInput").val();
-    var corporation = $("input:radio[name=corporation]:checked").val();
-    var build = $("input:radio[name=build]:checked").val();
-    var hero = $("input:radio[name=hero]:checked").val();
-    var fingers = $("input:radio[name=fingers]:checked").val();
-
-    event.preventDefault()
+  alert("works");
 
 
+//
+//     var game = $("#gameInput").val();
+//     var corporation = $("input:radio[name=corporation]:checked").val();
+//     var build = $("input:radio[name=build]:checked").val();
+//     var hero = $("input:radio[name=hero]:checked").val();
+//     var fingers = $("input:radio[name=fingers]:checked").val();
+//
+//     event.preventDefault()
+//
+//     $("li").hide();
+//
+//     // if (game === "1"
+//
+//
+//
+//
+//
+//
+//
 
 
+    event.preventDefault();
 
-
-
-
-  });
   });
 });
