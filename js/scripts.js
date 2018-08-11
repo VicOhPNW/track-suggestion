89369f5eebea79f67238dde1ad8af6afb92fd791
Voh <ohvictori@gmail.com>
2018-08-10 22:19:32 -0700

    Nothing to commit. Note that I've been editing to troubleshoot and determine why this is not working as expected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("button").click(function(event) {
 
-    var game = $("#gameInput").val();
+    var game = $("#inputGame").val();
     var corporation = $("input:radio[name=corporation]:checked").val();
     var build = $("input:radio[name=build]:checked").val();
     var hero = $("input:radio[name=hero]:checked").val();
@@ -11,18 +11,16 @@ $(document).ready(function(){
     // $("li").hide();
 
 // This alert works unsure what's going on below.-
-    alert("work");
+    // alert("work");
 
-// //Reult branching
-//     if (game === "1" && corporation === "3" && build === "6" && hero === "10" && fingers === "15") {
-//       $("#design").show();
-//     } else if (game === "2" && corporation === "4" && build === "7" && hero = "11" && fingers === "16") {
-//       $("#java").show();
-//     } else if
+// Result branching - user interface
+$("button").click(function(event) {
 
-
-
-
+    if (game === "1" && corporation === "3" && build === "6" && hero === "10" && fingers === "15") {
+      $("#cSharp").show();
+    // } else if (game === "2" && corporation === "4" && build === "7" && hero = "11" && fingers === "16") {
+    //   $("#java").show();
+    // } else if
 
 
 
@@ -30,7 +28,11 @@ $(document).ready(function(){
 
 
 
-    event.preventDefault();
+
+
+
+
+    // event.preventDefault();
 
   });
 });
