82c4433bc363f2b4a30ba6fdccbb0928ce240fb7
Victoria Oh <ohvictori@gmail.com>
2018-08-15 19:57:14 -0700

    Add photo of Radia Perlman. Add remaining photos to html. Clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $(".button").click(function(event) {
 
     var game = $("#inputGame").val();
@@ -7,25 +7,26 @@ $(document).ready(function(){
     var hero = $("input:radio[name=hero]:checked").val();
     var fingers = $("input:radio[name=fingers]:checked").val();
 
-
-
-// Result branching
+    // Result branching
     if (game === "1" && corporation === "1" && build === "1" && hero === "1" && fingers === "1") {
       $("#cSharp").show();
       $(".notResults").hide();
-      } else if (game === "2" && corporation === "2" && build === "2" && hero === "2" && fingers === "2") {
-        $("#java").show();
-      } else if (game === "1" && corporation === "3" && build === "3" && hero === "3" && fingers === "3") {
-        $("#php").show();
-      } else if (game === "2" && corporation === "1" && build === "1" && hero === "1" && fingers === "3") {
-        $("#php").show();
-      } else if (game === "2" && corporation === "3" && build === "3" && hero === "2" && fingers === "1") {
-        $("#ruby").show();
-      } else if (game === "2" && corporation === "1" && build === "3" && hero === "4" && fingers === "2")
-        $("#css").show();
-
+    } else if (game === "2" && corporation === "2" && build === "2" && hero === "2" && fingers === "2") {
+      $("#java").show();
+      $(".notResults").hide();
+    } else if (game === "1" && corporation === "3" && build === "3" && hero === "3" && fingers === "3") {
+      $("#php").show();
+      $(".notResults").hide();
+    } else if (game === "2" && corporation === "1" && build === "1" && hero === "1" && fingers === "3") {
+      $("#php").show();
+      $(".notResults").hide();
+    } else if (game === "2" && corporation === "3" && build === "3" && hero === "2" && fingers === "1") {
+      $("#ruby").show();
+      $(".notResults").hide();
+    } else if (game === "2" && corporation === "1" && build === "3" && hero === "4" && fingers === "2") $("#css").show();
+    $(".notResults").hide();
 
     event.preventDefault();
-
+    
   });
 });
