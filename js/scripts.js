550982f0bac78f81bbe892e8f852c40d5257fe50
Victoria Oh <ohvictori@gmail.com>
2018-08-15 18:45:45 -0700

    update class design to css.

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,28 @@ $(document).ready(function(){
     var hero = $("input:radio[name=hero]:checked").val();
     var fingers = $("input:radio[name=fingers]:checked").val();
 
-//Hide when remaining result options when result appears
+// Hide when remaining result options when result appears
     // $("li").hide();
 
 
 // Result branching
-
     if (game === "1" && corporation === "1" && build === "1" && hero === "1" && fingers === "1") {
-      $("#cSharp").show(); }
-    // } else if (game === "2" && corporation === "4" && build === "7" && hero = "11" && fingers === "16") {
-    //   $("#java").show();
-    // } else if
+      $("#cSharp").show();
+      } else if (game === "2" && corporation === "2" && build === "2" && hero === "2" && fingers === "2") {
+        $("#java").show();
+      } else if (game === "1" && corporation === "3" && build === "3" && hero === "3" && fingers === "3") {
+        $("#php").show();
+      } else if (game === "2" && corporation === "1" && build === "1" && hero === "1" && fingers === "3") {
+        $("#php").show();
+      } else if (game === "2" && corporation === "3" && build === "3" && hero === "2" && fingers === "1") {
+        $("#ruby").show();
+      } else (game === "2" && corporation === "1" && build === "3" && hero === "4" && fingers === "2")
+        $("#css").show();
 
 
 
 
-
-
-
-
-
-
-
-  event.preventDefault();
+    event.preventDefault();
 
   });
 });
